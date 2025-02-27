76abbf407dd84907fd8ff5dabdaa2178f5492929
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2025-02-27 14:11:33 +0100

    update example usage

--- src/example_usage.py
+++ src/example_usage.py
@@ -5,7 +5,7 @@ from src.torch_blockiness import calculate_image_blockiness, rgb_to_grayscale
 
 
 def main():
-    img = torchvision.io.read_image("example_images/unsplash.jpg")
+    img = torchvision.io.read_image("example_images/unsplash60.jpg")
     img_gray = rgb_to_grayscale(img)
     blockiness = calculate_image_blockiness(img_gray)
     blockiness_float = float(blockiness)
